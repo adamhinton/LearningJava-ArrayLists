aa453e3081c16cd0c53d74ae90d67c4170504239
adamd <adam.d.hinton@gmail.com>
2024-02-28 20:30:39 -0500

    Specific typing is a good idea. GroceryItem[] instead of Object[]

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -6,10 +6,22 @@ package dev.lpa;
 // More flexible than Array
 // Its capacity grows as elements are added; that's all handled BTS
 
+import java.util.Arrays;
+
+record GroceryItem (String name, String type, int count){
+    public GroceryItem(String name){
+        this(name,"DAIRY", 1);
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
 
-
+        GroceryItem[] groceryArray = new GroceryItem[2];
+        groceryArray[0] = new GroceryItem("milk");
+        groceryArray[1] = new GroceryItem("apples", "PRODUCE", 6);
+        groceryArray[2] = "5 oranges";
+        System.out.println(Arrays.toString(groceryArray));
 
     }
 }
